384d496a1ac25165e9bc7466a8ec1e8aacca6f48
Erik Lundin <erik@wub.io>
2013-01-10 10:40:49 +0000

    Organize, and extend, the test suite

--- jo_test.go
+++ jo_test.go
@@ -35,47 +35,63 @@ type parseTest struct {
 	events []event
 }
 
-var parseTests = []parseTest{
+var validPrimitives = []parseTest{
 	{`""`, []event{{1, StringStart}, {1, StringEnd}}},
 	{`"abc"`, []event{{1, StringStart}, {4, StringEnd}}},
 	{`"\u8bA0"`, []event{{1, StringStart}, {7, StringEnd}}},
 	{`"\u12345"`, []event{{1, StringStart}, {8, StringEnd}}},
 	{"\"\\b\\f\\n\\r\\t\\\\\"", []event{{1, StringStart}, {13, StringEnd}}},
-	{`"`, []event{{1, StringStart}, {0, SyntaxError}}},
-	{`"foo`, []event{{1, StringStart}, {3, None}, {0, SyntaxError}}},
-	{`'single'`, []event{{0, SyntaxError}}},
-	{`"\u12g8"`, []event{{1, StringStart}, {4, SyntaxError}}},
 
 	{`0`, []event{{1, NumberStart}, {0, NumberEnd}}},
 	{`123`, []event{{1, NumberStart}, {2, None}, {0, NumberEnd}}},
 	{`-10`, []event{{1, NumberStart}, {2, None}, {0, NumberEnd}}},
 	{`0.10`, []event{{1, NumberStart}, {3, None}, {0, NumberEnd}}},
 	{`12.03e+1`, []event{{1, NumberStart}, {7, None}, {0, NumberEnd}}},
-	{`-`, []event{{1, NumberStart}, {0, SyntaxError}}},
-	{`0.`, []event{{1, NumberStart}, {1, None}, {0, SyntaxError}}},
-	{`10e`, []event{{1, NumberStart}, {2, None}, {0, SyntaxError}}},
+	{`0.1e2`, []event{{1, NumberStart}, {4, None}, {0, NumberEnd}}},
+	{`1e1`, []event{{1, NumberStart}, {2, None}, {0, NumberEnd}}},
+	{`3.141569`, []event{{1, NumberStart}, {7, None}, {0, NumberEnd}}},
+	{`10000000000000e-10`, []event{{1, NumberStart}, {17, None}, {0, NumberEnd}}},
+	{`9223372036854775808`, []event{{1, NumberStart}, {18, None}, {0, NumberEnd}}},
+	{`6E-06`, []event{{1, NumberStart}, {4, None}, {0, NumberEnd}}},
+	{`1E-06`, []event{{1, NumberStart}, {4, None}, {0, NumberEnd}}},
 
 	{`false`, []event{{1, BoolStart}, {4, BoolEnd}}},
 	{`true`, []event{{1, BoolStart}, {3, BoolEnd}}},
-	{`t`, []event{{1, BoolStart}, {0, SyntaxError}}},
+	{`null`, []event{{1, NullStart}, {3, NullEnd}}},
+}
+
+var invalidPrimitives = []parseTest{
+	{`"`, []event{{1, StringStart}, {0, SyntaxError}}},
+	{`"foo`, []event{{1, StringStart}, {3, None}, {0, SyntaxError}}},
+	{`'single'`, []event{{0, SyntaxError}}},
+	{`"\u12g8"`, []event{{1, StringStart}, {4, SyntaxError}}},
+	{`"\u"`, []event{{1, StringStart}, {2, SyntaxError}}},
+	{`"you can\'t do this"`, []event{{1, StringStart}, {8, SyntaxError}}},
+
+	{`-`, []event{{1, NumberStart}, {0, SyntaxError}}},
+	{`0.`, []event{{1, NumberStart}, {1, None}, {0, SyntaxError}}},
+	{`123.456.789`, []event{{1, NumberStart}, {6, SyntaxError}}},
+	{`10e`, []event{{1, NumberStart}, {2, None}, {0, SyntaxError}}},
+	{`10e+`, []event{{1, NumberStart}, {3, None}, {0, SyntaxError}}},
+	{`10e-`, []event{{1, NumberStart}, {3, None}, {0, SyntaxError}}},
+	{`0e1x`, []event{{1, NumberStart}, {2, NumberEnd}, {0, SyntaxError}}},
+	{`0e+13.`, []event{{1, NumberStart}, {4, NumberEnd}, {0, SyntaxError}}},
+	{`0e+-0`, []event{{1, NumberStart}, {2, SyntaxError}}},
+
 	{`tr`, []event{{1, BoolStart}, {1, None}, {0, SyntaxError}}},
-	{`tru`, []event{{1, BoolStart}, {2, None}, {0, SyntaxError}}},
 	{`truE`, []event{{1, BoolStart}, {2, SyntaxError}}},
-	{`f`, []event{{1, BoolStart}, {0, SyntaxError}}},
-	{`fa`, []event{{1, BoolStart}, {1, None}, {0, SyntaxError}}},
-	{`fal`, []event{{1, BoolStart}, {2, None}, {0, SyntaxError}}},
 	{`fals`, []event{{1, BoolStart}, {3, None}, {0, SyntaxError}}},
 	{`fALSE`, []event{{1, BoolStart}, {0, SyntaxError}}},
-
-	{`null`, []event{{1, NullStart}, {3, NullEnd}}},
-	{`NULL`, []event{{0, SyntaxError}}},
 	{`n`, []event{{1, NullStart}, {0, SyntaxError}}},
-	{`nu`, []event{{1, NullStart}, {1, None}, {0, SyntaxError}}},
-	{`nul`, []event{{1, NullStart}, {2, None}, {0, SyntaxError}}},
+	{`NULL`, []event{{0, SyntaxError}}},
 }
 
 func TestParsing(t *testing.T) {
-	for _, test := range parseTests {
+	tests := make([]parseTest, 0)
+	tests = append(tests, validPrimitives...)
+	tests = append(tests, invalidPrimitives...)
+
+	for _, test := range tests {
 		input := []byte(test.json)
 		pos := 0
 

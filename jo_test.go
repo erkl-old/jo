68c5de83511321c3ac20bbcdfb40d82143f36db1
Erik Lundin <erik@wub.io>
2013-01-10 08:45:12 +0000

    Add a few test cases for syntax errors

--- jo_test.go
+++ jo_test.go
@@ -36,31 +36,42 @@ type parseTest struct {
 }
 
 var parseTests = []parseTest{
-	// strings
 	{`""`, []event{{1, StringStart}, {1, StringEnd}}},
 	{`"abc"`, []event{{1, StringStart}, {4, StringEnd}}},
+	{`"\u8bA0"`, []event{{1, StringStart}, {7, StringEnd}}},
+	{`"\u12345"`, []event{{1, StringStart}, {8, StringEnd}}},
+	{"\"\\b\\f\\n\\r\\t\\\\\"", []event{{1, StringStart}, {13, StringEnd}}},
+	{`"`, []event{{1, StringStart}, {0, SyntaxError}}},
+	{`"foo`, []event{{1, StringStart}, {3, None}, {0, SyntaxError}}},
+	{`'single'`, []event{{0, SyntaxError}}},
+	{`"\u12g8"`, []event{{1, StringStart}, {4, SyntaxError}}},
 
-	// numbers
+	{`0`, []event{{1, NumberStart}, {0, NumberEnd}}},
 	{`123`, []event{{1, NumberStart}, {2, None}, {0, NumberEnd}}},
+	{`-10`, []event{{1, NumberStart}, {2, None}, {0, NumberEnd}}},
+	{`0.10`, []event{{1, NumberStart}, {3, None}, {0, NumberEnd}}},
+	{`12.03e+1`, []event{{1, NumberStart}, {7, None}, {0, NumberEnd}}},
+	{`-`, []event{{1, NumberStart}, {0, SyntaxError}}},
+	{`0.`, []event{{1, NumberStart}, {1, None}, {0, SyntaxError}}},
+	{`10e`, []event{{1, NumberStart}, {2, None}, {0, SyntaxError}}},
 
-	// true
+	{`false`, []event{{1, BoolStart}, {4, BoolEnd}}},
+	{`true`, []event{{1, BoolStart}, {3, BoolEnd}}},
 	{`t`, []event{{1, BoolStart}, {0, SyntaxError}}},
 	{`tr`, []event{{1, BoolStart}, {1, None}, {0, SyntaxError}}},
 	{`tru`, []event{{1, BoolStart}, {2, None}, {0, SyntaxError}}},
-	{`true`, []event{{1, BoolStart}, {3, BoolEnd}}},
-
-	// false
+	{`truE`, []event{{1, BoolStart}, {2, SyntaxError}}},
 	{`f`, []event{{1, BoolStart}, {0, SyntaxError}}},
 	{`fa`, []event{{1, BoolStart}, {1, None}, {0, SyntaxError}}},
 	{`fal`, []event{{1, BoolStart}, {2, None}, {0, SyntaxError}}},
 	{`fals`, []event{{1, BoolStart}, {3, None}, {0, SyntaxError}}},
-	{`false`, []event{{1, BoolStart}, {4, BoolEnd}}},
+	{`fALSE`, []event{{1, BoolStart}, {0, SyntaxError}}},
 
-	// null
+	{`null`, []event{{1, NullStart}, {3, NullEnd}}},
+	{`NULL`, []event{{0, SyntaxError}}},
 	{`n`, []event{{1, NullStart}, {0, SyntaxError}}},
 	{`nu`, []event{{1, NullStart}, {1, None}, {0, SyntaxError}}},
 	{`nul`, []event{{1, NullStart}, {2, None}, {0, SyntaxError}}},
-	{`null`, []event{{1, NullStart}, {3, NullEnd}}},
 }
 
 func TestParsing(t *testing.T) {
